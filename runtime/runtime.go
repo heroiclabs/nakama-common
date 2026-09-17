c1c5ad8429c42d3028896a9ba88d55b973d7cdb2
Andrei Mihu <andrei@heroiclabs.com>
2026-09-17 19:48:07 +0100

    Add friends reset flag.

--- runtime/runtime.go
+++ runtime/runtime.go
@@ -1105,11 +1105,12 @@ AuthenticateProvider is a named authentication backend registered through Initia
 
 Authenticate receives the opaque JSON payload supplied by the caller that must be validated by the implementer of this interface.
 
-GetFriends is used to import friend relationships from the external provider, if available.
+GetFriends is used to import friend relationships from the external provider, if available. Also returns a boolean flag
+indicating if the user's existing friends list must be reset and completely replaced.
 */
 type AuthenticateProvider interface {
 	Authenticate(ctx context.Context, logger Logger, db *sql.DB, nk NakamaModule, payload map[string]any) (AuthenticateProviderResult, error)
-	GetFriends(ctx context.Context, logger Logger, db *sql.DB, nk NakamaModule, payload map[string]any, result AuthenticateProviderResult) ([]string, error)
+	GetFriends(ctx context.Context, logger Logger, db *sql.DB, nk NakamaModule, payload map[string]any, result AuthenticateProviderResult) ([]string, bool, error)
 }
 
 var _ AuthenticateProviderResult = (*DefaultAuthenticateProviderResult)(nil)
